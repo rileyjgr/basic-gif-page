fc9cbbd84a0beaff84fa986cc5ecf876c023123e
Riley Griffin <rigriffin241@gmail.com>
2018-07-28 13:37:51 -0400

    change some colors

--- assets/js/getGifs.js
+++ assets/js/getGifs.js
@@ -118,6 +118,35 @@ $("#dog-button").on("click", function() {
   });
 });
 
+//Dogs
+$("#dog-button").on("click", function() {
+
+  //api key with gif type
+  var queryURL = "https://api.giphy.com/v1/gifs/random?api_key=Jouf48SlZuDz7W7ODP6bYA6q5ZBEDcGV&tag=dog";
+
+  //
+  $.ajax({
+    url: queryURL,
+    method: "GET"
+  })
+
+  //
+  .then(function(response) {
+
+    //
+    var imageUrl = response.data.image_original_url;
+
+    //
+    var dogImage = $("<img>");
+
+    //
+    dogImage.attr("src", imageUrl);
+    dogImage.attr("alt", "dog image");
+
+    //
+    $("#images4").prepend(dogImage);
+  });
+});
 
 
 let goSearch = () => {
@@ -127,6 +156,112 @@ let goSearch = () => {
 
   });
 }
+
+//Random gif
+$("#random-button").on("click", function() {
+
+  //api key with gif type
+  var queryURL = "https://api.giphy.com/v1/gifs/random?api_key=Jouf48SlZuDz7W7ODP6bYA6q5ZBEDcGV&tag=random";
+
+  //
+  $.ajax({
+    url: queryURL,
+    method: "GET"
+  })
+
+  //
+  .then(function(response) {
+
+    //
+    var imageUrl = response.data.image_original_url;
+
+    //
+    var randomImage = $("<img>");
+
+    //
+    randomImage.attr("src", imageUrl);
+    randomImage.attr("alt", "random image");
+
+    //
+    $("#images5").prepend(randomImage);
+  });
+});
+
+//top of the week
+$("#hot-button").on("click", function() {
+
+  //api key with gif type
+  var queryURL = "https://api.giphy.com/v1/gifs/random?api_key=Jouf48SlZuDz7W7ODP6bYA6q5ZBEDcGV&tag=top-week";
+
+  //
+  $.ajax({
+    url: queryURL,
+    method: "GET"
+  })
+
+  //
+  .then(function(response) {
+
+    //
+    var imageUrl = response.data.image_original_url;
+
+    //
+    var topImage = $("<img>");
+
+    //
+    topImage.attr("src", imageUrl);
+    topImage.attr("alt", "top image");
+
+    //
+    $("#images6").prepend(topImage);
+  });
+});
+
+//top of the week
+$("#wtf-button").on("click", function() {
+
+  //api key with gif type
+  var queryURL = "https://api.giphy.com/v1/gifs/random?api_key=Jouf48SlZuDz7W7ODP6bYA6q5ZBEDcGV&tag=wtf";
+
+  //
+  $.ajax({
+    url: queryURL,
+    method: "GET"
+  })
+
+  //
+  .then(function(response) {
+
+    //
+    var imageUrl = response.data.image_original_url;
+
+    //
+    var wtfImage = $("<img>");
+
+    //
+    wtfImage.attr("src", imageUrl);
+    wtfImage.attr("alt", "wtf image");
+
+    //
+    $("#images7").prepend(wtfImage);
+  });
+});
+
+
+// this works but its lazy
+const clearPage = () => {
+  let clearButton = document.getElementById('clear-button');
+  location.reload();
+}
+
+/* let goSearch = () => {
+  $('#search-input').on('submit', function() {
+    var tag_Id= $(this).children('input[name=userInput]').val();
+    var tag_Item= $(this).find('input[name="input-button"]').text();
+
+  });
+} */
+
 /*
 //user input
 $("#input-button).on("click", function() {
